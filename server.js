8b4e1d143cebcd2ecaa6b7c8341f8bf5ee379195
Taylor Johnson <taylor.christian.johnson@gmail.com>
2015-12-09 19:46:01 +0000

    modified auth middleware

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = 8080;
 
 var middleware = {
     requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
+        console.log('Private route hit!');
         next();
     },
     logger: function (req, res, next) {
