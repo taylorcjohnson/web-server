136e02e08d75cbbdf536251e2913a4a063df6443
Taylor Johnson <taylor.christian.johnson@gmail.com>
2015-12-09 20:01:12 +0000

    Refactor middleware

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 8080;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('Private route hit!');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 // Application-level middleware (as opposed to route-level middleware)
 //app.use(middleware.requireAuthentication);
